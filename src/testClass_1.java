a86c06a2ed12d03c936705fd0e520f98eb99e1e4
Scopule <glebka.vershinin@gmail.com>
2021-10-26 14:38:17 +0700

    Added objectClass for testing

--- src/testClass_1.java
+++ src/testClass_1.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 public class testClass_1 {
     public static void main(String[] args){
 
+        objectClass newObject = new objectClass(13,"joe",14000);
+        System.out.println(newObject.totalIncome());
         //new code;
     }
     static void doShit(String inputString){
