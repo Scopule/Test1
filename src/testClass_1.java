c48dd63cc1d9e8c3d0ed20f11c9983673d3b6d86
Scopule <glebka.vershinin@gmail.com>
2021-10-26 14:08:28 +0700

    added //new code -line

--- src/testClass_1.java
+++ src/testClass_1.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 public class testClass_1 {
     public static void main(String[] args){
 
+        //new code;
     }
     static void doShit(String inputString){
         System.out.println(inputString + '!');
