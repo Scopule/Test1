b96bd74fb10b8022d601fac0c9810f2b1fca7541
Scopule <glebka.vershinin@gmail.com>
2021-10-26 14:02:56 +0700

    Initial commit

---
+++ src/testClass_1.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class testClass_1 {
+    public static void main(String[] args){
+
+    }
+    static void doShit(String inputString){
+        System.out.println(inputString + '!');
+    }
+    }
